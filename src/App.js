0c1eaad202e452f5d76b40a46ddd10e2f7042a78
jack <jackzhoumine@gmail.com>
2020-04-19 15:36:29 +0800

    第一次提交

---
+++ src/App.js
@@ -0,0 +1,34 @@
+/*
+ * @Description: 应用入口
+ * @Date: 2020-04-19 15:27:52
+ * @Author: JackChouMine
+ * @LastEditTime: 2020-04-19 15:32:00
+ * @LastEditors: JackChouMine
+ */
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1>hello react</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
