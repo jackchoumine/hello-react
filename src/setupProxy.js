555d91496ff163492b82676d41983d665a294569
jack <jackzhoumine@gmail.com>
2021-06-22 15:20:05 +0800

    chore: 配置多个代理

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,18 +2,21 @@
  * @Description: 代理配置
  * @Date: 2021-06-22 11:44:02 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-06-22 14:53:57 +0800
+ * @LastEditTime: 2021-06-22 15:19:03 +0800
  * @LastEditors: JackChou
  */
 const { createProxyMiddleware } = require('http-proxy-middleware')
-
+const proxy = createProxyMiddleware
 module.exports = function (app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:4000',
-      // changeOrigin: true,
-      pathRewrite: { '^/api': '' },
-    })
-  )
+  const dev1 = proxy('/api', {
+    target: 'http://localhost:4000',
+    changeOrigin: true,
+    pathRewrite: { '^/api': '' },
+  })
+  const dev2 = proxy('/api2', {
+    target: 'http://localhost:4000',
+    changeOrigin: true,
+    pathRewrite: { '^/api': '' },
+  })
+  app.use(dev1, dev2)
 }
